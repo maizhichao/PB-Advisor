79472cbedd1311c96e62ed30bb9d959bb7227a44
ZhiChao MAI <SESA525731@se.com>
2019-10-14 20:25:12 +0800

    Remove footprints of antd pro preview.

--- config/config.js
+++ config/config.js
@@ -5,7 +5,6 @@ import webpackPlugin from './plugin.config';
 const { pwa, primaryColor } = defaultSettings; // preview.pro.ant.design only do not use in your production ;
 // preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
 
-const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION } = process.env;
 const plugins = [
   [
     'umi-plugin-react',
@@ -316,8 +315,7 @@ export default {
     'primary-color': primaryColor,
   },
   define: {
-    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION:
-      ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION || '', // preview.pro.ant.design only do not use in your production ; preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
+    // Define env variables
   },
   ignoreMomentLocale: true,
   lessLoaderOptions: {
--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testURL: 'http://localhost:8000',
   preset: 'jest-puppeteer',
   globals: {
-    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: false,
+    // define env variables
   },
 };
